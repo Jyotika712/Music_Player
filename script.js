f63ba0cff005d40838b6fa2851921b1dcdbcb211
Jyotika Sachdeva <147473308+Jyotika712@users.noreply.github.com>
2024-06-29 11:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-let progress=document.getElementById("progress");
-let song=document.getElementById("song");
-let controlicon=document.getElementById("controlicon");
-song.onloadedmetadata=function(){
-    progress.max=song.duration;
-    progress.value=song.currentTime;
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controlicon = document.getElementById("controlicon");
 
-}
-function PlayPause(){
-    if(controlicon.classList.contains("fa-pause")){
-        song.pause();
-        controlicon.classList.remove("fa-pause");
-        controlicon.classList.add("fa-play")
-    }
-    else{
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+};
+
+function PlayPause() {
+    if (song.paused) {
         song.play();
         controlicon.classList.remove("fa-play");
-        controlicon.classList.add("fa-pause")
+        controlicon.classList.add("fa-pause");
+    } else {
+        song.pause();
+        controlicon.classList.remove("fa-pause");
+        controlicon.classList.add("fa-play");
     }
 }
-if(song.play()){
-    setInterval(()=>{
-        progress.value=song.currentTime;
-    },500);
-}
-progress.onchange=function(){
-    song.play();
-    song.currentTime=progress.value;
-    controlicon.classList.remove("fa-play");
-    controlicon.classList.add("fa-pause")
-}
+
+song.addEventListener('timeupdate', function() {
+    progress.value = song.currentTime;
+});
+
+progress.oninput = function() {
+    song.currentTime = progress.value;
+};
+
